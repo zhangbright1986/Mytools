e40c8be5084e098295617eefb4733fe500619cd5
bright <zhangbright1986@gmail.com>
2017-01-14 00:29:50 -0500

    added list comparison warning

--- psort.py
+++ psort.py
@@ -5,43 +5,52 @@ from ase.data import chemical_symbols
 import sys
 import argparse
 
-#filein='POSCAR'
-#fileout='POSCAR.new'
+# filein='POSCAR'
+# fileout='POSCAR.new'
 parser = argparse.ArgumentParser()
-parser.add_argument("filein",nargs="?", help='reading file',default='POSCAR')
-parser.add_argument("fileout",nargs="?", help='output file',default='POSCAR.new')
-parser.add_argument("-s", "--seq", nargs='+',help="elements order",default=None)
+parser.add_argument("filein", nargs="?", help='reading file', default='POSCAR')
+parser.add_argument("fileout", nargs="?", help='output file', default='POSCAR.new')
+parser.add_argument("-s", "--seq", nargs='+', help="elements order", default=None)
 args = parser.parse_args()
 print args.filein, args.fileout, args.seq
 
-print "Read structure from "+ args.filein
+print "Read structure from " + args.filein
 if args.seq:
-    print "Sorting strcuture in the order of " +str(args.seq) 
+    print "Sorting strcuture in the order of " + str(args.seq)
 else:
     print "Sorting strucutre in order of chemical symbols"
 print "Write sorted strcuture to " + args.fileout
 
-p=read(args.filein)
+p = read(args.filein)
+pcopy = p.copy()
+pcopy = sort(pcopy)
+plist = pcopy.get_chemical_symbols()
+# print plist
+plist = list(set(plist))
+rlist = list(set(args.seq))
 
 if args.seq:
-    potdict={}
-    val=0
+    potdict = {}
+    val = 0
+
+    if plist not in rlist:
+        print "Warning: Sorting list doesn't include all element in input file"
+
     for si in args.seq:
         if si in chemical_symbols:
-            print si, val   
-            potdict.update({si: val}) 
+            print si, val
+            potdict.update({si: val})
             val += 1
-        else: 
-            print si+" is not a chemical symbol and will be skipped"
-    
+        else:
+            print si + " is not a chemical symbol and will be skipped"
+
     for pi in p:
         for key in potdict:
             if pi.symbol == key:
                 pi.tag = potdict[key]
 
-    p=sort(p,tags=p.get_tags())
+    p = sort(p, tags=p.get_tags())
 else:
-    p=sort(p)
+    p = sort(p)
 
-write(args.fileout,p)
-
+write(args.fileout, p)
