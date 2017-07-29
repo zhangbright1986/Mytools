8caf110b46fe6b9369fa852e62a8874e08314017
Liang Zhang <liangzha@chestnut-login.internal>
2017-07-29 08:21:29 -0400

    more edit on espclean.py

--- espclean.py
+++ espclean.py
@@ -4,8 +4,11 @@ import os
 import sys
 from ase.io import read,write
 
-
-tar=sys.argv[1]
+try:
+    tar=sys.argv[1]
+except IndexError:
+    print "please give the name of directory"
+    exit()
 
 if not os.path.isdir('./'+tar):
     os.system('mkdir '+tar)
@@ -13,9 +16,11 @@ else:
     print tar+' exist !!!'
     exit()
 
-os.system('mv opt.log err* out* esp.log *node* relax.traj'+tar)
+os.system('mv -f err* out* esp.log *node* '+tar)
 os.system('cp init.traj fin.traj '+tar)
 os.system('rm init.traj')
 
 if not os.path.isfile('./fin.traj'):
-    os.system('cp '+tar+'/relax.traj init.traj')
+    os.system('cp relax.traj init.traj')
+    os.system('mv -f opt.log relax.traj ' +tar)
+   
