1e39b10e91d2195930b223b015faa2efd9db15f0
Liang Zhang <tg829713@login4.stampede2.tacc.utexas.edu>
2017-08-01 21:14:11 -0500

    update cleanvasp.py

--- cleanvasp.py
+++ cleanvasp.py
@@ -10,13 +10,14 @@ except IndexError:
     print "please give the name of the folder"
     exit()
     
-os.system('cp init.traj  ' + str(tar))
 os.system('vfin.pl '+str(tar))
+os.system('cp init.traj  ' + str(tar))
+
 if os.path.isfile('fin.traj'):
     os.system('cp  ' + str(tar) +'/fe.dat  .')
     os.system('rm init.traj')
 else:
-    ptmp=read('CONTCAR')
+    ptmp=read(str(tar)+'/CONTCAR')
     pini=read(str(tar)+'/init.traj')
     mag=pini.get_initial_magnetic_moments()
     ptmp.set_initial_magnetic_moments(mag)
