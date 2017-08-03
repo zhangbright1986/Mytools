ba67ef64b5e23bfcd297a74aa1b0dad0eb0c1294
Liang Zhang <zhangbright1986@gmail.com>
2017-08-03 07:13:05 -0700

    updagted cleanvasp.py

--- cleanvasp.py
+++ cleanvasp.py
@@ -4,9 +4,6 @@ from ase.io import read, write
 from ase.calculators.vasp import Vasp
 import sys
 
-calc = Vasp(restart=True)
-p= calc.get_atoms()
-write('fin.traj',p)
 
 try:
     tar=sys.argv[1]
@@ -14,13 +11,20 @@ except IndexError:
     print "please give the name of the folder"
     exit()
     
-os.system('vfin.pl '+str(tar))
-os.system('cp init.traj fin.traj ' + str(tar))
+
 
 if os.path.isfile('fin.traj'):
+    calc = Vasp(restart=True)
+    p= calc.get_atoms()
+    write('fin.traj',p)
+    os.system('vfin.pl '+str(tar))
+
+    os.system('cp init.traj fin.traj ' + str(tar))
     os.system('cp  ' + str(tar) +'/fe.dat  .')
     os.system('rm init.traj')
 else:
+    os.system('vfin.pl '+str(tar))
+    os.system('cp init.traj ' + str(tar))
     ptmp=read(str(tar)+'/CONTCAR')
     pini=read(str(tar)+'/init.traj')
     mag=pini.get_initial_magnetic_moments()
