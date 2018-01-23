325d2f20615fea0ceefd6c3223233f5064c78da0
Liang Zhang <liangzha@chestnut-login.internal>
2018-01-23 01:18:35 -0500

    edit

--- vasp2traj.py
+++ vasp2traj.py
@@ -18,34 +18,13 @@ if os.path.isfile(tar+'/OUTCAR'):
     os.chdir(tar)
     calc = Vasp(restart=True)
     p=calc.get_atoms()
+    print p.get_magnetic_moments()
     if hasattr(p, 'get_magnetic_moments'):
-        p.set_initial_magnetic_moments = p.get_magnetic_moments()
+        mag= p.get_magnetic_moments()
+        print mag
+        p.set_initial_magnetic_moments(mag)
 
 os.chdir(cwd)
 
 write('vasp.traj',p)
 
-    
-
-#p=calc.get_atoms()
-'''
-isf os.path.isfile('fin.traj'):
-    calc = Vasp(restart=True)
-    p= calc.get_atoms()
-    write('fin.traj',p)
-    os.system('vfin.pl '+str(tar))
-
-    os.system('cp init.traj fin.traj ' + str(tar))
-    os.system('cp  ' + str(tar) +'/fe.dat  .')
-    os.system('rm init.traj')
-else:
-    os.system('vfin.pl '+str(tar))
-    os.system('cp init.traj ' + str(tar))
-    ptmp=read(str(tar)+'/CONTCAR')
-    pini=read(str(tar)+'/init.traj')
-    mag=pini.get_initial_magnetic_moments()
-    ptmp.set_initial_magnetic_moments(mag)
-    write('init.traj', ptmp)
-
-os.system('mv err* out* '+ str(tar))
-'''
